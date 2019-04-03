4b11d9103d181ce83d93818fbb2f6e2b5f5ab713
jhonM8a <jhon.ochoa@unillanos.edu.co>
2019-04-03 21:03:56 +0000

    Se añde estilos al componenten que se creo

--- src/components/Badge.js
+++ src/components/Badge.js
@@ -1,25 +1,26 @@
 import React from 'react';
 import ConfLogo from '../images/badge-header.svg';
+import "./styles/Badge.css";
 
 class Badge extends React.Component{
     render(){   
         return(
-            <div>
-                <div>
+            <div className="Badge">
+                <div className="Badge__header">
                     <img src={ConfLogo} alt="Logo"/>
                 </div>
 
-                <div>
-                    <img src="https://es.gravatar.com/avatar?d=identicon" alt ="Aavatar"/>
+                <div className="Badge__section-name">
+                    <img className="Badge__avatar" src="https://es.gravatar.com/avatar?d=identicon" alt ="Aavatar"/>
                     <h1>Jhon</h1>
                 </div>
 
-                <div>
-                    <p>Ingenier fullstack</p>
-                    <p>@jochoa</p>
+                <div className="Badge__section-info">
+                    <h3>Ingenier fullstack</h3>
+                    <div>@jochoa</div>
                 </div>
 
-                <div>
+                <div className= "Badge__footer">
                     #ConConf
                 </div>
             </div>
