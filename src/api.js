4f75dbc65ccd2421fda758aade8daf035d044687
jhonochoa <m8alemus@gmail.com>
2019-04-09 21:02:08 -0500

    Se añade animación de Loading y se crea un componente para el erro

--- src/api.js
+++ src/api.js
@@ -24,7 +24,8 @@ async function callApi(endpoint, options = {}) {
 const api = {
   badges: {
     list() {
-      return callApi("/badges");
+      // return callApi("/badges");
+      throw new Error("500 server error");
     },
     create(badge) {
       return callApi(`/badges`, {
