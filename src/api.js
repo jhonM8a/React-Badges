de26ea02540613d7fd253d508de009d417c48e52
jhonochoa <m8alemus@gmail.com>
2019-04-09 21:54:28 -0500

    Se añade llamada post y se hacen cambios

--- src/api.js
+++ src/api.js
@@ -24,8 +24,9 @@ async function callApi(endpoint, options = {}) {
 const api = {
   badges: {
     list() {
-      // return callApi("/badges");
-      throw new Error("500 server error");
+      //
+      return callApi("/badges");
+      //throw new Error("500 server error");
     },
     create(badge) {
       return callApi(`/badges`, {
--- src/components/BadgeForm.js
+++ src/components/BadgeForm.js
@@ -1,54 +1,85 @@
-import React from 'react';
+import React from "react";
 
-class BadgeForm extends React.Component{
-    //state = {};
-    /*
+class BadgeForm extends React.Component {
+  //state = {};
+  /*
     handleChange = e =>{
         //console.log({e});
         this.setState({
             [e.target.name]:e.target.value,
         })
     };*/
-    handleclick = e =>{
-        console.log("Button");
-        
-    };
-    handleSubmit = e =>{
-        e.preventDefault(); //Evita que el formulario se envie
-        console.log(this.props.formValues);
-        
-    };
+  handleclick = e => {
+    console.log("Button");
+  };
+  // handleSubmit = e =>{
+  //     e.preventDefault(); //Evita que el formulario se envie
+  //     console.log(this.props.formValues);
 
-    render(){
-        return(
-            <div>
-                <h1>New Person</h1>
-                <form onSubmit={this.handleSubmit} className="">
-                    <div className="form-group">
-                        <label>Firts Name</label>
-                        <input onChange={this.props.onChange} className="form-control" type="text" name="firstName" value={this.props.formValues.firstName}/>
-                    </div>
-                    <div className="form-group">
-                        <label>Last Name</label>
-                        <input onChange={this.props.onChange} className="form-control" type="text" name="lastName" value={this.props.formValues.lastName}/>
-                    </div> 
-                    <div className="form-group">
-                        <label>E-mail</label>
-                        <input onChange={this.props.onChange} className="form-control" type="email" name="email" value={this.props.formValues.email}/>
-                    </div> 
-                    <div className="form-group">
-                        <label>Job Title</label>
-                        <input onChange={this.props.onChange} className="form-control" type="text" name="jobTitle" value={this.props.formValues.jobTitle}/>
-                    </div>   
-                    <div className="form-group">
-                        <label>Twitter</label>
-                        <input onChange={this.props.onChange} className="form-control" type="text" name="twitter" value={this.props.formValues.twitter}/>
-                    </div>                
-                    <button onClick={this.handleclick} className="btn btn-primary">Save</button>
-                </form>
-            </div>
-        )
-    }
+  // };
+
+  render() {
+    return (
+      <div>
+        <h1>New Person</h1>
+        <form onSubmit={this.props.onSubmit} className="">
+          <div className="form-group">
+            <label>Firts Name</label>
+            <input
+              onChange={this.props.onChange}
+              className="form-control"
+              type="text"
+              name="firstName"
+              value={this.props.formValues.firstName}
+            />
+          </div>
+          <div className="form-group">
+            <label>Last Name</label>
+            <input
+              onChange={this.props.onChange}
+              className="form-control"
+              type="text"
+              name="lastName"
+              value={this.props.formValues.lastName}
+            />
+          </div>
+          <div className="form-group">
+            <label>E-mail</label>
+            <input
+              onChange={this.props.onChange}
+              className="form-control"
+              type="email"
+              name="email"
+              value={this.props.formValues.email}
+            />
+          </div>
+          <div className="form-group">
+            <label>Job Title</label>
+            <input
+              onChange={this.props.onChange}
+              className="form-control"
+              type="text"
+              name="jobTitle"
+              value={this.props.formValues.jobTitle}
+            />
+          </div>
+          <div className="form-group">
+            <label>Twitter</label>
+            <input
+              onChange={this.props.onChange}
+              className="form-control"
+              type="text"
+              name="twitter"
+              value={this.props.formValues.twitter}
+            />
+          </div>
+          <button onClick={this.handleclick} className="btn btn-primary">
+            Save
+          </button>
+        </form>
+      </div>
+    );
+  }
 }
 
 export default BadgeForm;
